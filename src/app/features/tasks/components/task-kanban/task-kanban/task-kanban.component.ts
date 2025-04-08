2ab3ff81381896b7083fe63664557399e9b5ad03
irwan-cahyo-saputro <irwancahyos@gmail.com>
2025-04-08 18:22:30 +0700

    [talenavi-test][010] Upgrade angular from 18 to 19

--- src/app/features/tasks/components/task-kanban/task-kanban/task-kanban.component.ts
+++ src/app/features/tasks/components/task-kanban/task-kanban/task-kanban.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-task-kanban',
-  templateUrl: './task-kanban.component.html',
-  styleUrls: ['./task-kanban.component.scss']
+    selector: 'app-task-kanban',
+    templateUrl: './task-kanban.component.html',
+    styleUrls: ['./task-kanban.component.scss'],
+    standalone: false
 })
 export class TaskKanbanComponent {
 
--- src/app/features/tasks/components/task-table/task-table/task-table.component.ts
+++ src/app/features/tasks/components/task-table/task-table/task-table.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-task-table',
-  templateUrl: './task-table.component.html',
-  styleUrls: ['./task-table.component.scss']
+    selector: 'app-task-table',
+    templateUrl: './task-table.component.html',
+    styleUrls: ['./task-table.component.scss'],
+    standalone: false
 })
 export class TaskTableComponent {
 
